cc0ea85097c7e8821d4cf2e6a42b2294f5b5513d
austin-mino <minomin2437@gmail.com>
2025-07-10 16:48:24 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,39 @@
+// server.js
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 let players = {};
 
-app.use(express.static('public'));
-
 io.on('connection', socket => {
-  console.log('접속:', socket.id);
+  console.log('Connected:', socket.id);
 
   socket.on('join', data => {
-    players[socket.id] = { 
-      ...data, position: [0,0,0], quaternion: [0,0,0,1]
+    players[socket.id] = {
+      nickname: data.nickname,
+      carType: data.carType,
+      color: data.color,
+      position: [0, 0, 0],
+      quaternion: [0, 0, 0, 1],
     };
     io.emit('players', players);
   });
 
-  socket.on('update', dt => {
+  socket.on('update', data => {
     if (players[socket.id]) {
-      players[socket.id].position = dt.position;
-      players[socket.id].quaternion = dt.quaternion;
+      players[socket.id].position = data.position;
+      players[socket.id].quaternion = data.quaternion;
     }
     io.emit('players', players);
   });
@@ -34,4 +44,6 @@ io.on('connection', socket => {
   });
 });
 
-server.listen(3000, () => console.log('서버 3000번에서 실행 중'));
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
